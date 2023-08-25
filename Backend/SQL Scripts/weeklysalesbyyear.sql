f2fed07950a17b15400f68307e77ea8a8704e325
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-08-25 19:16:26 +0300

    final commit

--- Backend/SQL Scripts/weeklysalesbyyear.sql
+++ Backend/SQL Scripts/weeklysalesbyyear.sql
@@ -3,3 +3,17 @@ SELECT  MAX(weekly_sales) AS TotalWeeklySales, Date
   FROM [walmart sales].[dbo].[Walmart sales data]
   GROUP BY Date 
   ORDER BY TotalWeeklySales DESC;
+
+  /****** 2010-12-24  ******/
+
+  /****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [Store]
+      ,[Date]
+      ,[Weekly_Sales]
+      ,[Holiday_Flag]
+      ,[Temperature]
+      ,[Fuel_Price]
+      ,[CPI]
+      ,[Unemployment]
+  FROM [walmart sales].[dbo].[Walmart sales data]
+  WHERE Date='2010-12-24'
